f8b5d2ff21238e365e9d3a9de4b4a15e77f14772
eduhsu3 <eduhsu3@gmail.com>
2024-09-25 17:53:39 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Header from './components/header';
+import Header from './components/Header';
 import RepoList from './components/RepoList';
 
 function App() {
